3150198cf0a20349e3af45bca27615cb3ab0c524
Vahan Babayan <vahan3x@gmail.com>
2019-02-03 19:29:16 +0400

    Initial Commit

---
+++ WideGamutRendering/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WideGamutRendering
+//
+//  Created by Vahan Babayan on 2/3/19.
+//  Copyright © 2019 vahan3x. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
